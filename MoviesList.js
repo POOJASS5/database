e50273009e504b60a9a625f5460e11094e616001
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-21 12:27:33 +0530

    created form to post movies to database and created delete button to delete data

--- MoviesList.js
+++ MoviesList.js
@@ -1,21 +1,20 @@
 import React from "react";
-
 import Movie from "./Movie";
 import classes from "./MoviesList.module.css";
-
 const MoviesList = (props) => {
   return (
     <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
+          id={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          onDelete={props.onDelete}
         />
       ))}
     </ul>
   );
 };
-
 export default MoviesList;
